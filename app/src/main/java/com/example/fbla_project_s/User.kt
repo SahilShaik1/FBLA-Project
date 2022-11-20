c24130118e7b3f5c22e49f1c4a703e3c768f6add
Sahil <sahil.r.shaik@gmail.com>
2022-11-20 15:29:13 -0500

    commit

--- app/src/main/java/com/example/fbla_project_s/User.kt
+++ app/src/main/java/com/example/fbla_project_s/User.kt
@@ -1,3 +1,3 @@
 package com.example.fbla_project_s
 
-public data class User(val firstName : String? = null, val lastName : String? = null, val age: String? = null, val userName : String? = null)
+data class User(val firstName : String? = null, val lastName : String? = null, val age: String? = null, val userName : String? = null)
